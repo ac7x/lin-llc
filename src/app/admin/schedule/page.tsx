2aed3c5599f5c2baaffb2968d62760287d5c57ea
ac7x <ac7x@pm.me>
2025-05-26 09:01:03 +0800

    在 SchedulePage 中新增 TestTimelineControl 組件，以增強時間軸的控制功能，提升使用者互動體驗。

--- src/app/admin/schedule/page.tsx
+++ src/app/admin/schedule/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import TimelineComponent, { TimelineItem } from '../../../components/Timeline';
+import TestTimelineControl from '../../../components/TestTimelineControl';
 
 const SchedulePage: React.FC = () => {
   const [items, setItems] = useState<TimelineItem[]>([]);
@@ -9,6 +10,7 @@ const SchedulePage: React.FC = () => {
   return (
     <div>
       <h1>行程排程</h1>
+      <TestTimelineControl />
       <TimelineComponent items={items} onItemsChange={setItems} />
     </div>
   );
