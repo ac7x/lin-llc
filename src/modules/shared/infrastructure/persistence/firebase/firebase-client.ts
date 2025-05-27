9a99566e0aa90ef228f4a055eac2900323df8e36
ac7x <ac7x@pm.me>
2025-05-27 17:04:46 +0800

    更新 Firebase 配置以符合最新專案需求

--- src/modules/shared/infrastructure/persistence/firebase/firebase-client.ts
+++ src/modules/shared/infrastructure/persistence/firebase/firebase-client.ts
@@ -2,13 +2,13 @@ import { initializeApp, type FirebaseApp } from 'firebase/app'
 import { getFirestore, type Firestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDsJP6_bjWLQ0SQiarhe3UIApnqx60vCqg",
-  authDomain: "lin-llc-liff.firebaseapp.com",
-  projectId: "lin-llc-liff",
-  storageBucket: "lin-llc-liff.firebasestorage.app",
-  messagingSenderId: "734381604026",
-  appId: "1:734381604026:web:a07a50fe85c6c5acd25683",
-  measurementId: "G-KBMLTJL6KK"
+  apiKey: "AIzaSyCUDU4n6SvAQBT8qb1R0E_oWvSeJxYu-ro",
+  authDomain: "lin-llc.firebaseapp.com",
+  projectId: "lin-llc",
+  storageBucket: "lin-llc.firebasestorage.app",
+  messagingSenderId: "394023041902",
+  appId: "1:394023041902:web:f9874be5d0d192557b1f7f",
+  measurementId: "G-62JEHK00G8"
 }
 
 export const firebaseApp: FirebaseApp = initializeApp(firebaseConfig)
