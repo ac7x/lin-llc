292e3fe3af5f3e39fea4d3e2491c91f8fcee206a
ac7x <ac7x@pm.me>
2025-05-27 18:04:28 +0800

    新增 Firebase 認證功能，導入 getAuth 方法以初始化 Firebase 認證服務，並更新 firebase-client.ts 檔案以包含認證實例。

--- src/modules/shared/infrastructure/persistence/firebase/firebase-client.ts
+++ src/modules/shared/infrastructure/persistence/firebase/firebase-client.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, type FirebaseApp } from 'firebase/app'
 import { getFirestore, type Firestore } from 'firebase/firestore'
+import { getAuth, type Auth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCUDU4n6SvAQBT8qb1R0E_oWvSeJxYu-ro",
@@ -16,3 +17,5 @@ export const firebaseApp: FirebaseApp =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
 
 export const firestore: Firestore = getFirestore(firebaseApp)
+
+export const auth: Auth = getAuth(firebaseApp)
