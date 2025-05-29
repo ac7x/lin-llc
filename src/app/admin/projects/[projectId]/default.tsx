6b59c05eafd2df4ac42295088971f8e6bcecbd53
ac7x <ac7x@pm.me>
2025-05-30 03:12:49 +0800

    修正 Default 組件，移除 OverviewPage 渲染，返回 null

--- src/app/admin/projects/[projectId]/default.tsx
+++ src/app/admin/projects/[projectId]/default.tsx
@@ -1,8 +1,3 @@
-import OverviewPage from "./@overview/page";
-
 export default function Default() {
-    // 渲染 OverviewPage 作為此路由區段的預設內容。
-    // 這有助於 Next.js 正確解析基礎路徑，
-    // 進而使其能夠處理啟動平行路由插槽的子路徑。
-    return <OverviewPage />;
+    return null;
 }
