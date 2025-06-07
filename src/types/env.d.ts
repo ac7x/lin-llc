eee01436db298f220d0c777f1382986fbb0ae4c3
ac7x <ac7x@pm.me>
2025-06-07 22:29:14 +0800

    刪除不再使用的環境變數、Firebase 鉤子和工具函數，並更新 tsconfig.json 配置

--- src/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY: string;
-    GOOGLE_RECAPTCHA_SECRET_KEY: string;
-  }
-}
