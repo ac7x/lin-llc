977bd12cc0c49fa2a1c702382ac24d8e6fba093a
ac484 <ac7x7x@gmail.com>
2025-06-25 22:09:28 +0800

    1

--- src/app/project/components/tree/index.ts
+++ src/app/project/components/tree/index.ts
@@ -1,11 +1,6 @@
 /**
-<<<<<<< HEAD
  * 專案樹狀組件統一導出檔案
  * 提供所有層級的樹狀結構組件
-=======
- * 專案樹狀結構組件統一導出檔案
- * 提供所有層級的樹狀結構顯示組件
->>>>>>> f90a68f84c32d43cdd71c140b301b9bc75114c54
  */
 
 export { default as ProjectTree } from './project-tree';
